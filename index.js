182f5c853f0c0eaa146774f5bb44cfea9c5b6098
josemiguelhen <107695767+josemiguelhen@users.noreply.github.com>
2023-02-07 23:20:44 -0300

    cambios

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const productManager = require('./ProductManager/ProductManager');
+const productos = require('./data/products.json');
+const prod = new productManager(productos);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
+app.get('/products', async (req, res)=>{
+    console.log(req.query);
+    const {limit} = req.query;
+    if(limit){
+        res.send(productos.slice(0, +limit))
+    }else{
+        res.send({productos});
+    }
+})
+
+
+app.get('/products/:pid', async (req, res)=>{
+    console.log(req.params);
+    const prodId = req.params.pid;
+    const id = productos.find(prod => prod.id === +prodId);
+    if (!id) {
+      return res.status(404).send("Product not found");
+    }
+    res.send({id});
+})
+
+app.get('/', async (req,res)=>{
+    console.log('Show products');
+    res.send(prod);
+    
+})
+
+app.listen(8080, () => console.log('Server is up and running on port 8080'))
