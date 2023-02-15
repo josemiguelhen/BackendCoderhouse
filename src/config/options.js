6677cd3c4cbe2385a8496bb64c68a67caf1948e5
josemiguelhen <107695767+josemiguelhen@users.noreply.github.com>
2023-02-15 12:21:20 -0300

    arreglo

---
+++ src/config/options.js
@@ -0,0 +1,10 @@
+const options = {
+    fileSystem: {
+        productsFileName: "products.json"
+    },
+    mongoDB:{
+        url: "mongodb+srv://josem:CWd4vaXIN1PSgdN6@codercluster.yy22mql.mongodb.net/ecommerce?retryWrites=true&w=majority"
+    }
+}
+
+export default options
